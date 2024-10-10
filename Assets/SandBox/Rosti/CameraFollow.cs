ec5d4ef23e795d7061f09f3799b79eb433dd2e22
Rostyslav Bobrakov <rbobrakov@edu.cdv.pl>
2024-10-10 12:56:43 +0200

    MovementAndCameraRotation

---
+++ Assets/SandBox/Rosti/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset = new Vector3(0, 2, -5);
+    public float smoothSpeed = 0.125f;
+
+    void Start()
+    {
+        transform.position = target.position + offset; 
+        transform.LookAt(target);
+    }
+
+    void LateUpdate()
+    {
+        Vector3 desiredPosition = target.position + offset;
+
+        transform.position = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+
+        transform.LookAt(target);
+    }
+}
